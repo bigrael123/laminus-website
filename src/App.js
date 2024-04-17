539c722b5ec01f0cde05bf9533f845270b8b9cd8
bigrael123 <bigrael-x@hotmail.com>
2024-04-17 17:18:54 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import logo from './logo.jpeg';
 import './App.css';
-
+import { Link } from "react-router-dom"
 function App() {
+  const home = () => {
+    document.getElementById("futurefeed").innerHTML = "This will be a feed in the future";
+  }
+  const about = () => {
+    document.getElementById("futurefeed").innerHTML = "This will be about us";
+  }
+  const Games = () => {
+    document.getElementById("futurefeed").innerHTML = "Here is where our Games will be displayed";
+  }
+  const Career = () => {
+    document.getElementById("futurefeed").innerHTML = "Jobs will be listed here";
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <header className="App">
+      <img src={logo} className="Logo" alt='Logo' />
+      <div id='home'>
+      <Link onClick={home} className='Home'>Home</Link>
+      <Link onClick={Games} className='Games'>Software</Link>
+      <Link onClick={Career} className='Career'>Career</Link>
+      <Link onClick={about} className='About'>About Us</Link>
+      </div>
+      
+
+    </header>
+    <div id='test'> <p id='futurefeed'>This will be a feed in the future</p></div>
     </div>
   );
 }
